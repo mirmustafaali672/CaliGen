85f6faf73c7e78f5e280bf0283aac63464b3b03a
mirmustafaali672 <mirmustafa672@gmail.com>
2024-03-14 17:13:10 +0530

    added menu logic

--- data/MenuData.tsx
+++ data/MenuData.tsx
@@ -2,51 +2,89 @@ export type MenuListInterface = {
   name: string;
   navigationRouteName: string;
   icons: string;
-  isParent: int;
+  isParent: boolean;
   parentName: string;
+  ChildComponents: any[];
 };
 
 export const MenuList: MenuListInterface = [
   {
-    name: 'Menu Item One',
+    name: 'Application Setup',
     navigationRouteName: 'HomeScreen',
-    icons: 'none',
-    isParent: 1,
+    icons: 'stream',
+    isParent: false,
     parentName: 'none',
   },
   {
-    name: 'Menu Item Two',
+    name: 'Data Auto Exploration',
     navigationRouteName: 'HomeScreen',
-    icons: 'none',
-    isParent: 1,
+    icons: 'project-diagram',
+    isParent: true,
+    parentName: 'none',
+    ChildComponents: [
+      {
+        name: 'subs',
+        navigationRouteName: 'HomeScreen',
+        icons: 'project-diagram',
+        isParent: false,
+        parentName: 'none',
+      },
+      {
+        name: 'Kanban',
+        navigationRouteName: 'HomeScreen',
+        icons: 'clipboard-list',
+        isParent: false,
+        parentName: 'none',
+      },
+      {
+        name: 'Mene',
+        navigationRouteName: 'HomeScreen',
+        icons: 'none',
+        isParent: false,
+        parentName: 'none',
+      },
+    ],
+  },
+  {
+    name: 'Kanban',
+    navigationRouteName: 'HomeScreen',
+    icons: 'clipboard-list',
+    isParent: false,
     parentName: 'none',
   },
   {
-    name: 'Menu Item Three',
+    name: 'Procedures',
     navigationRouteName: 'HomeScreen',
-    icons: 'none',
-    isParent: 1,
+    icons: 'route',
+    isParent: false,
     parentName: 'none',
   },
   {
-    name: 'Menu Item Four',
-    navigationRouteName: 'HomeScreen',
-    icons: 'none',
-    isParent: 1,
-    parentName: 'none',
-  },
-  {
-    name: 'Menu Item Five',
+    name: 'Skills',
     navigationRouteName: 'none',
-    icons: 'none',
-    isParent: 1,
+    icons: 'book',
+    isParent: false,
     parentName: 'none',
   },
   {
-    name: 'Menu Item Six',
+    name: 'Schedule',
     navigationRouteName: 'none',
-    icons: 'none',
-    isParent: 1,
+    icons: 'calendar-alt',
+    isParent: false,
+    parentName: 'none',
+  },
+  {
+    name: 'History',
+    navigationRouteName: 'none',
+    icons: 'history',
+    isParent: false,
+    parentName: 'none',
+  },
+  {
+    name: 'About',
+    navigationRouteName: 'none',
+    icons: 'info-circle',
+    isParent: false,
     parentName: 'none',
   },
 ];
--- screens/MoreScreen/index.tsx
+++ screens/MoreScreen/index.tsx
@@ -6,43 +6,206 @@ import {
   FlatList,
   TouchableOpacity,
   StyleSheet,
+  ScrollView,
 } from 'react-native';
 import {MenuList} from '../../data/MenuData';
-import { PrimaryColor, PrimaryBGColor } from '../../styles/primaryScreenColors';
-import PrimaryButton  from '../../components/Buttons/PrimaryButtonComponent';
-function MenuScreen({navigation}) {
+import {PrimaryColor, PrimaryBGColor} from '../../styles/primaryScreenColors';
+import PrimaryButton from '../../components/Buttons/PrimaryButtonComponent';
+import * as MaterialColors from '../../styles/materialColors';
+import RobotoText from '../../components/Text/RobotoText';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
-    function navigateTo({ route })
-    {
-        navigation.navigate('HomeScreen');
-    }
-  return (
-    <View style={{flex: 1, margin: 5, backgroundColor: PrimaryBGColor}}>
-      <SafeAreaView style={{flex: 1}}>
-        <FlatList
-          keyExtractor={item => item.name}
-          data={MenuList}
-          renderItem={({item}) => {
-            return (
-              <View  style={styles.MenuButtonStyle}>
-                <PrimaryButton  buttonClicked={ () => navigateTo(item.navigationRouteName)} buttonTitle={item.name}/>
+function GetMenuView() {
+  return MenuList.map(item => (
+    <View style={{flex: 1, backgroundColor: MaterialColors.MaterialWhite}}>
+      <SafeAreaView style={{flex: 1, margin: 6}}>
+        <View>
+          <TouchableOpacity>
+            <View
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                width: '100%',
+                height: 30,
+                marginVertical: 5,
+                borderBottomWidth: 0,
+                borderColor: MaterialColors.MaterialBlueGreyLight,
+                flexDirection: 'row',
+                flex: 1,
+              }}>
+              <View style={{flex: 1}}>
+                <FontAwesome5
+                  name={item.icons}
+                  color={MaterialColors.MaterialBlueGreyLight}
+                  size={12}
+                />
               </View>
-            );
-          }}
-        />
+              <View style={{flex: 10}}>
+                <RobotoText
+                  text={item.name}
+                  textStyle={{
+                    color: MaterialColors.MaterialBlack,
+                  }}
+                />
+              </View>
+            </View>
+          </TouchableOpacity>
+          {item.isParent && (
+            <SafeAreaView style={{flex: 1, margin: 10}}>
+              {item.ChildComponents.map(childItem => (
+                <View
+                  style={{
+                    flex: 1,
+                    backgroundColor: MaterialColors.MaterialWhite,
+                  }}>
+                  <SafeAreaView style={{flex: 1, marginHorizontal: 30}}>
+                    <View>
+                      <TouchableOpacity>
+                        <View
+                          style={{
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            width: '100%',
+                            height: 30,
+                            marginVertical: 5,
+                            borderBottomWidth: 0,
+                            borderColor: MaterialColors.MaterialBlueGreyLight,
+                            flexDirection: 'row',
+                            flex: 1,
+                          }}>
+                          <View style={{flex: 1}}>
+                            <FontAwesome5
+                              name={childItem.icons}
+                              color={MaterialColors.MaterialBlueGreyLight}
+                              size={12}
+                            />
+                          </View>
+                          <View style={{flex: 10}}>
+                            <RobotoText
+                              text={childItem.name}
+                              textStyle={{
+                                color: MaterialColors.MaterialBlack,
+                              }}
+                            />
+                          </View>
+                        </View>
+                      </TouchableOpacity>
+                    </View>
+                  </SafeAreaView>
+                </View>
+              ))}
+            </SafeAreaView>
+          )}
+        </View>
       </SafeAreaView>
     </View>
+  ));
+}
+
+function MenuScreen({navigation}) {
+  function navigateTo({route}) {
+    navigation.navigate('HomeScreen');
+  }
+  return (
+    <View>
+      <ScrollView>
+        <GetMenuView />
+      </ScrollView>
+    </View>
   );
+  // <View style={{flex: 1, backgroundColor: MaterialColors.MaterialWhite}}>
+  //   <SafeAreaView style={{flex: 1, margin: 10}}>
+  //           <View>
+  //           <TouchableOpacity>
+  //             <View
+  //               style={{
+  //                 alignItems: 'center',
+  //                 justifyContent: 'center',
+  //                 width: '100%',
+  //                 height: 30,
+  //                 marginVertical: 5,
+  //                 borderBottomWidth: 0,
+  //                 borderColor: MaterialColors.MaterialBlueGreyLight,
+  //                 flexDirection: 'row',
+  //                 flex: 1,
+  //               }}>
+  //               <View style={{flex: 1}}>
+  //                 <FontAwesome5
+  //                   name={item.icons}
+  //                   color={MaterialColors.MaterialBlueGreyLight}
+  //                   size={12}
+  //                 />
+  //               </View>
+  //               <View style={{flex: 10}}>
+  //                 <RobotoText
+  //                   text={item.name}
+  //                   textStyle={{
+  //                     color: MaterialColors.MaterialBlack,
+  //                   }}
+  //                 />
+  //               </View>
+  //             </View>
+  //           </TouchableOpacity>
+  //           { item.isParent && (
+  //             <SafeAreaView style={{flex: 1, margin: 10}}>
+  //             <FlatList
+  //               keyExtractor={item => item.name}
+  //               data={item.ChildComponents}
+  //               renderItem={({Item}) => {
+  //                 return (
+  //                   <View>
+  //                   <TouchableOpacity>
+  //                     <View
+  //                       style={{
+  //                         alignItems: 'center',
+  //                         justifyContent: 'center',
+  //                         width: '100%',
+  //                         height: 30,
+  //                         marginVertical: 5,
+  //                         borderBottomWidth: 0,
+  //                         borderColor: MaterialColors.MaterialBlueGreyLight,
+  //                         flexDirection: 'row',
+  //                         flex: 1,
+  //                       }}>
+  //                       <View style={{flex: 1}}>
+  //                         <FontAwesome5
+  //                           name={item.icons}
+  //                           color={MaterialColors.MaterialBlueGreyLight}
+  //                           size={12}
+  //                         />
+  //                       </View>
+  //                       <View style={{flex: 10}}>
+  //                         <RobotoText
+  //                           text={item.name}
+  //                           textStyle={{
+  //                             color: MaterialColors.MaterialBlack,
+  //                           }}
+  //                         />
+  //                       </View>
+  //                     </View>
+  //                   </TouchableOpacity>
+  //                   {/* { childItem.isParent && (
+
+  //                   )} */}
+  //                   </View>
+  //                 );
+  //               }}
+  //             />
+  //           </SafeAreaView>
+  //           )}
+  //           </View>
+  //   </SafeAreaView>
+  // </View>
 }
 
 const styles = StyleSheet.create({
   MenuButtonStyle: {
-    alignItems: "center",
+    alignItems: 'center',
     padding: 10,
     margin: 2,
-    marginHorizontal: 10, 
+    marginHorizontal: 10,
     backgroundColor: PrimaryColor,
-    borderRadius: 10
+    borderRadius: 10,
   },
 });
 
