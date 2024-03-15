6e2a250a4a75504aeaea3804b052e0b466401189
mirmustafaali672 <mirmustafa672@gmail.com>
2024-03-15 13:25:33 +0530

    added

--- components/Text/RobotoText.tsx
+++ components/Text/RobotoText.tsx
@@ -1,11 +1,19 @@
-import { Text, StyleSheet } from 'react-native';
+import {Text, StyleSheet} from 'react-native';
 
-function RobotoText({ text, textStyle, isBold, numberOfLines }){
-    return <Text  numberOfLines={ numberOfLines ? numberOfLines: 0 } style={[styles.title, textStyle, { fontFamily: isBold ? "Roboto-Bold" : "Roboto-Regular"}]}>{text}</Text>
+function RobotoText({text, textStyle, isBold, numberOfLines}) {
+  return (
+    <Text
+      numberOfLines={numberOfLines ? numberOfLines : 0}
+      style={[
+        styles.title,
+        textStyle,
+        {fontFamily: isBold ? 'Roboto-Bold' : 'Roboto-Regular'},
+      ]}>
+      {text}
+    </Text>
+  );
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
 
 export default RobotoText;
